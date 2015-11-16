ebd593781f17cc31b8dad11ba207ec03c251915e
kogrego <kogrego@gmail.com>
2015-11-16 13:45:41 +0200

    RecyclerView

--- app/src/main/java/il/ac/shenkar/grisha/todolist/MainActivity.java
+++ app/src/main/java/il/ac/shenkar/grisha/todolist/MainActivity.java
@@ -16,35 +16,12 @@ public class MainActivity extends Activity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    public ArrayList<String> getListItems() {
-        return listItems;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listItems = new ArrayList<>();
-        /*listItems.add("Mock Task 1");
-        listItems.add("Mock Task 2");
-        listItems.add("Mock Task 3");
-        listItems.add("Mock Task 4");
-        listItems.add("Mock Task 5");
-        listItems.add("Mock Task 6");
-        listItems.add("Mock Task 7");
-        listItems.add("Mock Task 8");
-        listItems.add("Mock Task 9");
-        listItems.add("Mock Task 10");
-        listItems.add("Mock Task 11");
-        listItems.add("Mock Task 12");
-        listItems.add("Mock Task 13");
-        listItems.add("Mock Task 14");
-        listItems.add("Mock Task 15");
-        listItems.add("Mock Task 16");
-        listItems.add("Mock Task 17");
-        listItems.add("Mock Task 18");
-        listItems.add("Mock Task 19");
-        listItems.add("Mock Task 20");*/
         mAdapter = new MyRecyclerAdapter(listItems);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -73,7 +50,7 @@ public class MainActivity extends Activity {
 
         if (requestCode == 1) {
             if(resultCode == Activity.RESULT_OK){
-                String result=data.getStringExtra("result");
+                String result = data.getStringExtra("result");
                 listItems.add(result);
             }
         }
